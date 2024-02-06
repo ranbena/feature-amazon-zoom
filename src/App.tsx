59d8c053a92313bd18935968e82977664073ff77
Ran Byron <ranbena@gmail.com>
2024-02-06 15:33:28 +0100

    adapted to video version

--- src/App.tsx
+++ src/App.tsx
@@ -29,29 +29,7 @@ const App: React.FC = () => {
               position={position}
             />
           )}
-          <h1>Batman (2011-2016) #24 (Batman (2011-))</h1>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
-          </p>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
-          </p>
+          <h1>This is a title</h1>
           <p>
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard dummy text
@@ -64,7 +42,6 @@ const App: React.FC = () => {
             PageMaker including versions of Lorem Ipsum.
           </p>
         </div>
-        <div />
       </section>
     </div>
   );
